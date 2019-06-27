4a7631b68fd66b9e58b2044801ea246e358f21f1
rickybeeloco <reekardoe@yahoo.com>
2019-06-27 13:21:00 -0700

    TEST

--- testing/testing.js
+++ testing/testing.js
@@ -1,7 +1,43 @@
-import React from 'react';
+// import React, { Component } from 'react'
 
-const Testing = () => {
-    return (  );
-}
- 
-export default Testing;
+// import Toolbar from './components/Toolbar/Toolbar'
+// import SideDrawer from './components/SideDrawer/SideDrawer'
+// import Backdrop from './components/Backdrop/Backdrop'
+
+// class App extends Component {
+//   state = {
+//     sideDrawerOpen: false,
+//   }
+
+//   drawerToggleClickHandler = () => {
+//     this.setState(prevState => {
+//       return { sideDrawerOpen: !prevState.sideDrawerOpen }
+//     })
+//   }
+
+//   backdropClickHandler = () => {
+//     this.setState({ sideDrawerOpen: false })
+//   }
+
+//   render() {
+//     let backdrop
+//     let sideDrawer
+
+//     if (this.state.sideDrawerOpen) {
+//       backdrop = <Backdrop click={this.backdropClickHandler} />
+//       sideDrawer = <SideDrawer />
+//     }
+//     return (
+//       <div style={{ height: '100%' }}>
+//         <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+//         {sideDrawer}
+//         {backdrop}
+//         <main style={{ marginTop: '64px' }}>
+//           <p>This is the page content!</p>
+//         </main>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
