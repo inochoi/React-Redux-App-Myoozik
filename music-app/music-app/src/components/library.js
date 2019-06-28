46d7c38867681192dbb44612226614eb18ed9ca1
Ino <choiihkp@gmail.com>
2019-06-28 00:59:19 -0700

    Update library.js

--- music-app/music-app/src/components/library.js
+++ music-app/music-app/src/components/library.js
@@ -1,104 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { getSongs } from "../actions/playlists";
+import { Link } from "react-router-dom";
 
-import { getSongs } from '../actions';
 
-const Library = (props) => {
+const Library = props => {
+  const initialState = {
+    playingUrl: "",
+    audio: null,
+    playing: false
+  };
 
-    const initialState = {
-        playingUrl: '',
-        audio: null,
-        playing: false        
-    }
-    
-    const [music, setMusic] = useState(initialState);    
+  const [music, setMusic] = useState(initialState);
 
-    useEffect (() => {
- 
-        props.getSongs();
-        console.log('line 19');
-        console.log(props);
-    }, []);
+  useEffect(() => {
+    props.getSongs();
+    console.log("line 19");
+    console.log(props.songList);
+  }, []);
 
-    const playMusic = (previewUrl) => {
-        console.log('play');
-        let audio = new Audio(previewUrl);
-        let newMusic = {};
-        if (!music.playing){
-            audio.play();
-            
-            newMusic.playing = true;
-            newMusic.playingUrl = previewUrl;
-            newMusic.audio = audio;
-        }
-        else if (music.playingUrl === previewUrl){
-            music.audio.pause();
-            newMusic.playing = false;
-        }
-        else {
-            music.audio.pause();
-            audio.play();
-            newMusic.playing = true;
-            newMusic.playingUrl = previewUrl;
-            newMusic.audio = audio;
-        }           
+  const playMusic = previewUrl => {
+    console.log("play");
+    let audio = new Audio(previewUrl);
+    let newMusic = {};
+    if (!music.playing) {
+      audio.play();
 
-        setMusic(newMusic);
-
-        audio.addEventListener("ended", function() 
-        {
-             console.log('music ended');
-             setMusic(initialState);
-        });
-        
+      newMusic.playing = true;
+      newMusic.playingUrl = previewUrl;
+      newMusic.audio = audio;
+    } else if (music.playingUrl === previewUrl) {
+      music.audio.pause();
+      newMusic.playing = false;
+    } else {
+      music.audio.pause();
+      audio.play();
+      newMusic.playing = true;
+      newMusic.playingUrl = previewUrl;
+      newMusic.audio = audio;
     }
 
-    return props.isLoading ?
-        <h2>Loading...</h2> :
-        ( 
-        <div> 
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Song</th>
-                        <th>Playback(sec)</th>
-                        <th>Album Name</th>
-                        <th>Artist</th>
-                        <th>Image</th>
-                        {/* <th>Play</th> */}
-                        <th>Play</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.songList !== undefined && props.songList.map((song, index) => (
-                        <tr key={index}>
-                            <td>{song.name}</td>
-                            <td>{song.playbackSeconds}</td>
-                            <td>{song.albumName}</td>
-                            <td>{song.artistName}</td>
-                            <td><img src= {`${song.imagesrc}`} alt='undefined' /></td>
-                            {/* <td><i className="fas fa-caret-right" onClick={() => playMusic(song.previewURL)}></i></td> */}
-                            <td>{music.playingUrl === song.previewURL
-                                ? <span className="toPlay" onClick={() => playMusic(song.previewURL)}> | |</span>
-                                : <span className="toPlay" onClick={() => playMusic(song.previewURL)} >&#9654;</span>}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table> 
-        </div>
-     );
-}
- 
+    setMusic(newMusic);
+
+    audio.addEventListener("ended", function() {
+      console.log("music ended");
+      setMusic(initialState);
+    });
+  };
+
+  return props.isLoading ? (
+    <h2>Loading...</h2>
+  ) : (
+    <div>
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Album</th>
+            <th>Artist</th>
+            <th>Cover</th>
+            <th>Play</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.songList !== undefined &&
+            props.songList.map((song, index) => (
+              <tr key={index}>
+                <td className="text-truncate">
+                  <Link to={`/library/${song.id}`}>{song.name}</Link>
+                </td>
+                <td className="text-truncate">{song.albumName}</td>
+                <td>{song.artistName}</td>
+                <td>
+                  <img src={`${song.imagesrc}`} alt="undefined" />
+                </td>
+                {/* <td><audio src={`${song.previewURL}`} controls /></td> */}
+                <td>
+                  {music.playingUrl === song.previewURL ? (
+                    <span
+                      className="toPlay"
+                      onClick={() => playMusic(song.previewURL)}
+                    >
+                      <button className="btn btn-outline-warning text-center playSong">
+                        <i className="fas fa-pause playSongIcon" />
+                      </button>
+
+                      {/* <i id="pause" className="fas fa-pause-circle" /> */}
+                    </span>
+                  ) : (
+                    <span
+                      className="toPlay"
+                      onClick={() => playMusic(song.previewURL)}
+                    >
+                      <button className="btn btn-outline-warning text-center playSong">
+                        <i className="fas fa-play playSongIcon" />
+                      </button>
+
+                      {/* <i id="play" className="fas fa-play-circle" /> */}
+                    </span>
+                  )}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 const mapStateToProps = state => ({
-    songList: state.data,
-    isLoading: state.isLoading
-    }
-)
+  songList: state.music.data,
+  isLoading: state.music.isLoading
+});
 
 const mapDispatchToProps = dispatch => ({
-    getSongs: () => dispatch(getSongs())
-})
+  getSongs: () => dispatch(getSongs())
+});
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Library);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Library);
