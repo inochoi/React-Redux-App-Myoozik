a1666023e95830f88c92c81ee3b9e83561f35fe3
inochoi <47161831+inochoi@users.noreply.github.com>
2019-08-13 14:31:15 -0700

    update

--- blog solution/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import PostsReducer from './reducer_posts';
-
-const rootReducer = combineReducers({
-  posts: PostsReducer, //<-- Posts
-});
-
-export default rootReducer;
--- testing/testing.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Testing = () => {
-    return ( <div>Test</div> );
-}
- 
-export default Testing;
